33388e2429fd3d31960b83576bb72ba1dbf569c1
rockyweng75 <wengsc@yuntech.edu.tw>
2022-04-20 20:25:45 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+//  import TheWelcome from '@/components/TheWelcome.vue'
+  import drawflow from '@/components/drawflow.vue'
+
+</script>
+
+<template>
+  <main>
+    <!-- <div id="canvas"></div>
+    <TheWelcome /> -->
+    <drawflow/>
+
+  </main>
+</template>
